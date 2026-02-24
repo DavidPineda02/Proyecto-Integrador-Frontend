6ca54540ee26374fcbc63d21aaca23515638d14a
DavidPineda02 <davxpa.02@gmail.com>
2026-02-24 17:55:39 -0500

    Reestructurar scripts/js/ a scripts/utils/ y scripts/validations/ con barrel files

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -1,3 +1,2 @@
 export { crearMarcadoParrafos } from './marcado.js';
-export { validar } from './validaciones.js';
 export { obtenerClaseEstado, obtenerEtiquetaEstado, obtenerTextoFechaHoraActual, obtenerInicialesUsuario } from './tareas.js';
